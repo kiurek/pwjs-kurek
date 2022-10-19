2a9410561f1ecccd3859a2f7354b45e8f4218f47
kiurek <kubixk21@gmail.com>
2022-10-19 19:22:30 +0200

    lab2 slider

---
+++ lab2/main.js
@@ -0,0 +1,24 @@
+const main = document.querySelector('main')
+const timeoutRef = setTimeout(
+    () => { 
+        main.innerHTML='From setTimeout'
+},
+ 2000
+ )
+
+let licznik = 0
+const intervalRef = setInterval(
+    () => {
+        main.innerHTML="From interval" + licznik ++
+    },
+    4000
+)
+
+// kasowanie setInterval
+clearInterval(intervalRef)
+
+// kasowanie setTimeout
+clearTimeout(intervalRef)
+
+
+// window.requestAnimationFrame
