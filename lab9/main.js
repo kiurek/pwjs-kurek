15d75964bb4042eef7656b91f55b17a4bfad8415
kiurek <kubixk21@gmail.com>
2023-01-07 23:00:15 +0100

    lab9 done

--- lab9/main.js
+++ lab9/main.js
@@ -4,22 +4,40 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
-            + "&units=metric&appid="
+            + "&lang=pl&units=metric&appid="
             + this.apiKey
-        ).then((response) => response.json())
-        .then((data) => console.log(data));
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temperature, humidity } = data.main;
+        const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temperature, humidity, speed);
-        document.querySelector(".city").innerText = "Weather in" + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".city").innerText = "Pogoda w: " + name;
+        //document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
         document.querySelector(".temperature-description").innerText = description;
-        document.querySelector(".temperature").innerText = temperature + "°C";
-        document.querySelector(".temperature-humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector("temperature-wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".temperature").innerText = temp + "°C";
+        document.querySelector(".temperature-humidity").innerText = "Wilgotność: " + humidity + "%";
+        document.querySelector(".temperature-wind").innerText = "Prędkość wiatru: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+
+document
+    .querySelector(".search button")
+    .addEventListener('click', function () {
+        weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Brzesko")
